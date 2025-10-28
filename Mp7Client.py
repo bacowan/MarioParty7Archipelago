4a2b94ddb054f8e01f9b61114e1317471f9c0acb
bacowan <cowan.brendan.a@gmail.com>
2025-10-28 20:28:36 +0900

    hooking into dolphin

--- Mp7Client.py
+++ Mp7Client.py
@@ -2,14 +2,129 @@ from CommonClient import gui_enabled, logger, get_base_parser, ClientCommandProc
     CommonContext, server_loop
 import asyncio
 import Utils
+import dolphin_memory_engine
+import traceback
+from typing import TYPE_CHECKING, Any, Optional
+
+CONNECTION_REFUSED_GAME_STATUS = (
+    "Dolphin failed to connect. Please load a ROM for Mario Party 7. Trying again in 5 seconds..."
+)
+CONNECTION_REFUSED_SAVE_STATUS = (
+    "Dolphin failed to connect. Please load into the save file. Trying again in 5 seconds..."
+)
+CONNECTION_LOST_STATUS = (
+    "Dolphin connection was lost. Please restart your emulator and make sure Mario Party 7 is running."
+)
+CONNECTION_CONNECTED_STATUS = "Dolphin connected successfully."
+CONNECTION_INITIAL_STATUS = "Dolphin connection has not been initiated."
+
+class MarioParty7CommandProcessor(ClientCommandProcessor):
+    """
+    Command Processor for Mario Party 7 client commands.
+
+    This class handles commands specific to Mario Party 7.
+    """
+
+    def __init__(self, ctx: CommonContext):
+        """
+        Initialize the command processor with the provided context.
+
+        :param ctx: Context for the client.
+        """
+        super().__init__(ctx)
+
+    def _cmd_dolphin(self) -> None:
+        """
+        Display the current Dolphin emulator connection status.
+        """
+        if isinstance(self.ctx, MarioParty7Context):
+            logger.info(f"Dolphin Status: {self.ctx.dolphin_status}")
 
 class MarioParty7Context(CommonContext):
+    command_processor = MarioParty7CommandProcessor
     game = "Mario Party 7"
+    items_handling: int = 0b111
 
-def main(connect= None, password= None):
+    def __init__(self, server_address: Optional[str], password: Optional[str]) -> None:
+        super().__init__(server_address, password)
+        self.dolphin_sync_task: Optional[asyncio.Task[None]] = None
+        self.dolphin_status: str = CONNECTION_INITIAL_STATUS
+        self.awaiting_rom: bool = False
+
+    async def server_auth(self, password_requested: bool = False) -> None:
+        """
+        Authenticate with the Archipelago server.
+
+        :param password_requested: Whether the server requires a password. Defaults to `False`.
+        """
+        if password_requested and not self.password:
+            await super().server_auth(password_requested)
+        if not self.auth:
+            if self.awaiting_rom:
+                return
+            self.awaiting_rom = True
+            logger.info("Awaiting connection to Dolphin to get player information.")
+            return
+        await self.send_connect()
+
+async def dolphin_sync_task(ctx: MarioParty7Context) -> None:
+    logger.info("Starting Dolphin connector. Use /dolphin for status information.")
+    sleep_time = 0.0
+    while not ctx.exit_event.is_set():
+        if sleep_time > 0.0:
+            try:
+                # ctx.watcher_event gets set when receiving ReceivedItems or LocationInfo, or when shutting down.
+                await asyncio.wait_for(ctx.watcher_event.wait(), sleep_time)
+            except asyncio.TimeoutError:
+                pass
+            sleep_time = 0.0
+        ctx.watcher_event.clear()
+
+        try:
+            if dolphin_memory_engine.is_hooked() and ctx.dolphin_status == CONNECTION_CONNECTED_STATUS:
+                sleep_time = 0.1
+                dolphin_memory_engine.write_byte(0x80290DB7, 10)
+                if ctx.slot is not None:
+                    # wait for checks to come in to give to player
+                    pass
+                elif ctx.awaiting_rom:
+                    await ctx.server_auth()
+            else:
+                if ctx.dolphin_status == CONNECTION_CONNECTED_STATUS:
+                    logger.info("Connection to Dolphin lost, reconnecting...")
+                    ctx.dolphin_status = CONNECTION_LOST_STATUS
+                logger.info("Attempting to connect to Dolphin...")
+                dolphin_memory_engine.hook()
+                if dolphin_memory_engine.is_hooked():
+                    logger.info(dolphin_memory_engine.read_bytes(0x80000000, 6))
+                    if dolphin_memory_engine.read_bytes(0x80000000, 6) != b"GP7E01":
+                        logger.info(CONNECTION_REFUSED_GAME_STATUS)
+                        ctx.dolphin_status = CONNECTION_REFUSED_GAME_STATUS
+                        dolphin_memory_engine.un_hook()
+                        sleep_time = 5
+                    else:
+                        logger.info(CONNECTION_CONNECTED_STATUS)
+                        ctx.dolphin_status = CONNECTION_CONNECTED_STATUS
+                        ctx.locations_checked = set()
+                else:
+                    logger.info("Connection to Dolphin failed, attempting again in 5 seconds...")
+                    ctx.dolphin_status = CONNECTION_LOST_STATUS
+                    await ctx.disconnect()
+                    sleep_time = 5
+                    continue
+        except Exception:
+            dolphin_memory_engine.un_hook()
+            logger.info("Connection to Dolphin failed, attempting again in 5 seconds...")
+            logger.error(traceback.format_exc())
+            ctx.dolphin_status = CONNECTION_LOST_STATUS
+            await ctx.disconnect()
+            sleep_time = 5
+            continue
+
+def main(connect: Optional[str] = None, password: Optional[str] = None) -> None:
     Utils.init_logging("MarioParty7Client", exception_logger="Client")
 
-    async def _main(connect, password):
+    async def _main(connect: Optional[str], password: Optional[str]) -> None:
         ctx = MarioParty7Context(connect, password)
         ctx.server_task = asyncio.create_task(server_loop(ctx), name="ServerLoop")
         if gui_enabled:
@@ -17,13 +132,21 @@ def main(connect= None, password= None):
         ctx.run_cli()
         await asyncio.sleep(1)
 
-        # game start...
+        ctx.dolphin_sync_task = asyncio.create_task(dolphin_sync_task(ctx), name="DolphinSync")
+
+        await ctx.exit_event.wait()
+        # Wake the sync task, if it is currently sleeping, so it can start shutting down when it sees that the
+        # exit_event is set.
+        ctx.watcher_event.set()
 
         await ctx.exit_event.wait()
         ctx.server_address = None
 
         await ctx.shutdown()
 
+        if ctx.dolphin_sync_task:
+            await asyncio.sleep(3)
+            await ctx.dolphin_sync_task
 
     import colorama
 
