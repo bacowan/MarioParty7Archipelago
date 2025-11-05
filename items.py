44c348ffc11035b2f0dd21a659057e334b370a9f
bacowan <cowan.brendan.a@gmail.com>
2025-11-05 21:52:58 +0900

    creating new items

--- items.py
+++ items.py
@@ -10,14 +10,34 @@ class MarioParty7Item(Item):
         super().__init__(name, item_classifications[name], item_name_to_id[name], world.player)
 
 def create_items(world: World) -> None:
-    world.multiworld.itempool.append(MarioParty7Item("10 dice block", world))
+    world.multiworld.itempool.append(MarioParty7Item("Progressive Dice Block", world))
 
 progression_items = [
-    "10 dice block"
+    "Progressive Dice Block",
+    "Progressive Wallet",
+    "Shop Menu Navigation",
+    "Star Purchase Menu Navigation",
+    "Board Event Menu Navigation",
+    "Minigame Jump",
+    "Minigame Run",
+    "Minigame Mash"
+]
+
+useful_items = [
+    "Progressive Capsule Capacity"
+]
+
+filler_items = [
+    "1 Coin",
+    "5 Coins",
+    "10 Coins",
+    "Random Capsule"
 ]
 
 item_classifications = {
     **{ name: ItemClassification.progression for name in progression_items },
+    **{ name: ItemClassification.useful for name in useful_items },
+    **{ name: ItemClassification.filler for name in filler_items }
 }
 
 item_name_to_id = {name: address for address, name in enumerate(item_classifications, 1)}
