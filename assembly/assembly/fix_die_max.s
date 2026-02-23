d7a5bb0b79034beca86f268f795273f445e1b924
bacowan <cowan.brendan.a@gmail.com>
2026-02-23 22:47:55 +0900

    naming registers

--- assembly/assembly/fix_die_max.s
+++ assembly/assembly/fix_die_max.s
@@ -12,21 +12,24 @@ addi r3, r1, 44
 lhax r0, r3, r0
 
 # load the current player number into r18
+.equ r_CURRENT_PLAYER_NUMBER, r18
 lis r17, PLAYER_NUMBER_LOC@h
-lbz r18, PLAYER_NUMBER_LOC@l(r17)
+lbz r_CURRENT_PLAYER_NUMBER, PLAYER_NUMBER_LOC@l(r17)
 
 # the player number will act as an offset from p1's info structure address. Structures are offset by 0x110 bytes.
 # r18 will now store the full offset.
-mulli r18, r18, PLAYER_STRUCT_SIZE
+.equ r_CURRENT_PLAYER_OFFSET, r18
+mulli r_CURRENT_PLAYER_OFFSET, r_CURRENT_PLAYER_NUMBER, PLAYER_STRUCT_SIZE
 
 # load the cpu difficulty/is-player value into memory
+.equ r_CURRENT_PLAYER_DIFFICULTY, r17
 lis r17, PLAYER_STRUCT_BASE_OFFSET@h
 ori r17, r17, PLAYER_STRUCT_BASE_OFFSET@l
-add r17, r17, r18 # add the offset to the base memory value
-lbz r17, 0(r17)
+add r17, r17, r_CURRENT_PLAYER_OFFSET # add the offset to the base memory value
+lbz r_CURRENT_PLAYER_DIFFICULTY, 0(r17)
 
 # if it is a computer player, then skip to the end
-andi. r17, r17, IS_CPU_MASK_BIT # this bit will be set for CPUs but not human players
+andi. r17, r_CURRENT_PLAYER_DIFFICULTY, IS_CPU_MASK_BIT # this bit will be set for CPUs but not human players
 bne end
 
 # dice values are as follows:
@@ -36,35 +39,37 @@ bne end
 # 0b 0000 0011 # can roll normally
 
 # load the max dice roll value
+.equ r_MAX_DICE_ROLL_ENUM, r17
 lis r17, MAX_DICE_ROLL_SAVE@h
 ori r17, r17, MAX_DICE_ROLL_SAVE@l
-lbz r17, 0(r17)
+lbz r_MAX_DICE_ROLL_ENUM, 0(r17)
 
 # can only roll 1
-cmpwi r17, 0
+.equ r_DICE_ROLL, r0
+cmpwi r_MAX_DICE_ROLL_ENUM, 0
 bne roll2
-li r0, 0
+li r_DICE_ROLL, 0
 b end
 
 # can only roll 1 or 2
 roll2:
-cmpwi r17, 1
+cmpwi r_MAX_DICE_ROLL_ENUM, 1
 bne roll5
 li r19, 1
-and r0, r0, r19 # only take the last bit so that we get 0 or 1 (which gets one added to it)
+and r_DICE_ROLL, r_DICE_ROLL, r19 # only take the last bit so that we get 0 or 1 (which gets one added to it)
 b end
 
 # can only roll 1-5
 roll5:
-cmpwi r17, 2
+cmpwi r_MAX_DICE_ROLL_ENUM, 2
 bne end
 li r19, 1
-srw r0, r0, r19 # bit shift right to divide by 2
+srw r_DICE_ROLL, r_DICE_ROLL, r19 # bit shift right to divide by 2
 
 end:
 
 # set the value in memory (this is copied from the callsite)
-sth r0, 0x0054(r29)
+sth r_DICE_ROLL, 0x0054(r29)
 
 # go back to the callsite
 lis r18, RETURN_LINE@h
