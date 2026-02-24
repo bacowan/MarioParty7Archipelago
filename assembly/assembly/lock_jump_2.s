7a3d1efd001536799690baf5308b0aa015a1e132
bacowan <cowan.brendan.a@gmail.com>
2026-02-24 22:35:29 +0900

    refactoring lock_jump_2.s

--- assembly/assembly/lock_jump_2.s
+++ assembly/assembly/lock_jump_2.s
@@ -1,18 +1,26 @@
+.include "macros.inc"
+
+.equ RETURN_ADDRESS, 0x800A2734
+.equ MINIGAME_ENDED_VALUE, 0x804F2440
+.equ CURRENT_MINIGAME_OFFSET, 0x80291559
+.equ r_MINIGAME_ENDED_VALUE, r3
+.equ r_MINIGAME_END_FLAG, r29
+.equ r_CURRENT_MINIGAME_OFFSET, r3
+.equ r_ZERO, r15
+
 # when a minigame ends, set the current minigame to 0. The game seems to keep it at the original value for some reason,
 # but if we do this then we can tell if we're in a minigame or not.
 # This code appears to be called when a scene change happens. When a minigame ends, the value in r29
 # appears to be 0x804F2440
-lis         r3, 0x804F
-ori         r3, r3, 0x2440
+SET_REGISTER r_MINIGAME_ENDED_VALUE, MINIGAME_ENDED_VALUE
 
-cmpw        r3, r29
+cmpw        r_MINIGAME_ENDED_VALUE, r_MINIGAME_END_FLAG
 bne         end
 
 # the value at this memory address stores the id of the current minigame:
-lis         r3, 0x8029
-ori         r3, r3, 0x1559
-li          r15, 0
-stb         r15, 0(r3)
+SET_REGISTER r_CURRENT_MINIGAME_OFFSET, CURRENT_MINIGAME_OFFSET
+li          r_ZERO, 0
+stb         r_ZERO, 0(r_CURRENT_MINIGAME_OFFSET)
 
 end:
 
@@ -20,7 +28,7 @@ end:
 addi        r3, r29, 0
 
 # go back to the callsite
-lis         r15, 0x800A
-ori         r15, r15, 0x2734
+lis         r15, RETURN_ADDRESS@h
+ori         r15, r15, RETURN_ADDRESS@l
 mtctr       r15
 bctr
